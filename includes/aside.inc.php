9b781de95e30359498983367e353cc3b6205fc02
Admors <admors2309@gmail.com>
2025-07-13 19:03:26 +0400

    Add Dashboard Interface & Aside

---
+++ includes/aside.inc.php
@@ -0,0 +1,18 @@
+<?php ?>
+<!-- Aside -->
+<aside class="col-md-3 col-lg-2 mb-4">
+    <div class="list-group shadow sticky-top">
+        <a href="dashboard.php" class="list-group-item list-group-item-action active"><i class="bi bi-speedometer2"></i> Dashboard</a>
+        <a href="players.php" class="list-group-item list-group-item-action"><i class="bi bi-people"></i> Players</a>
+        <a href="vehicles.php" class="list-group-item list-group-item-action"><i class="bi bi-car-front"></i> Vehicles</a>
+        <a href="gangs.php" class="list-group-item list-group-item-action"><i class="bi bi-shield-shaded"></i> Gangs</a>
+        <a href="houses.php" class="list-group-item list-group-item-action"><i class="bi bi-house-door"></i> Houses</a>
+        <a href="logs.php" class="list-group-item list-group-item-action"><i class="bi bi-journal-text"></i> Logs</a>
+        <a href="reimbursements.php" class="list-group-item list-group-item-action"><i class="bi bi-cash-coin"></i> Reimbursement Logs</a>
+        <a href="notes.php" class="list-group-item list-group-item-action"><i class="bi bi-sticky"></i> Notes</a>
+        <a href="staff.php" class="list-group-item list-group-item-action"><i class="bi bi-person-badge"></i> Staff</a>
+        <a href="steam_accounts.php" class="list-group-item list-group-item-action"><i class="bi bi-steam"></i> Steam Accounts</a>
+        <hr class="my-1">
+        <a href="logout.php" class="list-group-item list-group-item-action text-danger"><i class="bi bi-box-arrow-right"></i> Log Out</a>
+    </div>
+</aside>
