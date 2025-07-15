4393f12b3bccbc5412227f5593bc5f5b02dfca77
Admors <admors2309@gmail.com>
2025-07-15 09:03:15 +0400

    Refactor components and includes

--- components/header.com.php
+++ components/header.com.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/lang.inc.php';
+require_once __DIR__ . '/../includes/lang.inc.php';
 
 $allowed_langs = ['French', 'English'];
 $lang = isset($_GET['lang']) && in_array($_GET['lang'], $allowed_langs) ? $_GET['lang'] : 'French'; // Exemple : ?lang=English
